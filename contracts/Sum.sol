8469ae0fa9f3430fb894911598700eaefd614c96
carmona44 <dcacarmona44@gmail.com>
2022-11-04 23:40:20 +0100

    add contract logic

---
+++ contracts/Sum.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Sum {
+    function sum(uint num1, uint num2) internal pure returns (uint) {
+        return num1 + num2;
+    }
+}
